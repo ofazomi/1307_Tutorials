067558372266f7c83a0441ab434cee7995b9a54d
ofazomi <ofazomi@gmail.com>
2013-07-16 13:32:56 -0700

    Create 5_5.rb

---
+++ 5_5.rb
@@ -0,0 +1,11 @@
+puts 'Hello there, and what\'s your name?'
+name = gets
+puts 'Your fine name is ' + name + '? What an odd name!'
+puts 'Pleased to meet you, ' + name + '. :)'
+puts 'Sorry about the name comment, ' + name + '. I meant \'odd\' in a good way.'
+
+puts ''
+
+puts 'Hello there again, and what\'s your alias now?'
+name = gets.chomp
+puts 'Very well, ' + name + '. We\'ll call you \'' + name + '\' that for the time being. What choice do I have? It\'s all I have in memory.'
