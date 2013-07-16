05f25a4e5f2fea2da2cb1ea01da4965ad080c7e3
ofazomi <ofazomi@gmail.com>
2013-07-16 12:56:19 -0700

    Create 4_0.rb

---
+++ 4_0.rb
@@ -0,0 +1,28 @@
+my_string = '...you can say that again...'
+puts my_string
+puts my_string
+puts ''
+puts my_string*2
+puts ''
+name = 'John Jacob Jingle Heimer Schmidt'
+puts name + '; His name is my name too.'
+puts 'Whenever I go out, the people always shout, "Hey, ' + name + '!"'
+puts ''
+compser = 'Mozart'
+puts composer + ' was "da bomb" in his day.'
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ', personally.'
+puts ''
+my_own_var = 'just another' + 'string'
+puts my_own_var
+my_own_var = 5 * (1+2)
+puts my_own_var
+puts ''
+var1 = 8
+var2 = var1
+puts var1
+puts var2
+puts ''
+var1 = 'eight'
+puts var1
+puts var2
