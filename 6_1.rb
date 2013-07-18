84cb369028586072406e3a7f7d4bfd3d67aff0da
ofazomi <ofazomi@gmail.com>
2013-07-17 18:13:13 -0700

    Update 6_1.rb

--- 6_1.rb
+++ 6_1.rb
@@ -27,3 +27,35 @@ puts ''
 lettersName = firstName.length.to_i + middleName.length.to_i + lastName.length.to_i
 puts '... um, ok. There are ' + lettersName.to_s + ' letters in your name.'
 puts 'Ta da! Be impressed.'
+
+puts ''
+
+letters = 'aAbBcCdDeE'
+puts letters.upcase
+puts letters.downcase
+puts letters.swapcase
+puts letters.capitalize
+puts ' a'.capitalize
+puts letters
+
+puts ''
+
+line_width = 50
+puts ('Old Mother Hubbard'.center(line_width))
+puts ('Sat in her cupboard'.center(line_width))
+puts ('Eating her curds and whey'.center(line_width))
+puts ('When along came a tiger'.center(line_width))
+puts ('Who sat down beside her'.center(line_width))
+puts ('And Ol\' Mother wondered'.center(line_width))
+puts ('How the hell...'.center(line_width))
+puts ('I mean, the door was closed.'.center(line_width))
+
+puts ''
+
+line_width = 40
+str = '--> text <--'
+puts(str.ljust(line_width))
+puts(str.center(line_width))
+puts(str.rjust(line_width))
+puts(str.ljust(line_width/2) + str.rjust(line_width/2))
+
