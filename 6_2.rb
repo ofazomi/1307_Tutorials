67c2f8fe964de1155c5adfc12c1efb651a5ebd3c
ofazomi <ofazomi@gmail.com>
2013-07-17 19:23:54 -0700

    Update 6_2.rb

--- 6_2.rb
+++ 6_2.rb
@@ -3,3 +3,10 @@ demand = gets.chomp
 puts 'WHADDAYA MEAN \'' + demand.upcase + '\'?! YOU\'RE FIRED!' 
 
 puts ''
+
+line_width = 56
+
+puts('Table of Contents'.center(line_width))
+puts('Chapter 1: Getting Started'.ljust(line_width/2) + 'page  1'.rjust(line_width/2))
+puts('Chapter 2: Numbers'.ljust(line_width/2) + 'page  9'.rjust(line_width/2))
+puts('Chapter 3: Letters'.ljust(line_width/2) + 'page 13'.rjust(line_width/2))
