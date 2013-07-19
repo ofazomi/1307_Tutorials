ca150cf487f3d97dba0c8c1af2cf0450199e80b9
ofazomi <ofazomi@gmail.com>
2013-07-19 00:26:20 -0700

    Create 7_3.rb

---
+++ 7_3.rb
@@ -0,0 +1,22 @@
+while 'Elodio' > 'Bertine'
+  puts 'what?'
+	input = gets.chomp
+	
+	if input != 'bye'
+		puts input
+		else
+			break
+		end
+	endb y
+
+puts 'kay, bye.'
+
+
+puts 'what else before you go?'
+
+input = ''
+while input != 'bye'
+puts input
+input = gets.chomp
+end
+puts 'Come again soon!'
