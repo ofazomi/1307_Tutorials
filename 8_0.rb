770d097bd8f0217b7cab5a4f93270320e924fdde
ofazomi <ofazomi@gmail.com>
2013-07-19 22:13:27 -0700

    Create 8_0.rb

---
+++ 8_0.rb
@@ -0,0 +1,15 @@
+names = ['Emily', 'Ryan', 'Donna', 'Dave']
+puts names
+puts
+puts names [0]
+puts names [1]
+puts names [2]
+puts names [3]
+puts names [4]
+
+other_peeps = []
+other_peeps[3] = 'Cicero'
+other_peeps[0] = 'Ceasar'
+other_peeps[1] = 'Ovid'
+other_peeps[0] = 'Augustus Ceasar'
+puts other_peeps
