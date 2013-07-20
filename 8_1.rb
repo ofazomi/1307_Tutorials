c078b3f729881633b8663da794f9accab9d5ff2d
ofazomi <ofazomi@gmail.com>
2013-07-19 22:12:40 -0700

    Create 8_1.rb

---
+++ 8_1.rb
@@ -0,0 +1,21 @@
+languages = ['English', 'Danish', 'Ruby']
+
+languages.each do |lang|
+  puts 'I love you ' + lang + '!'
+	puts 'Don\'t you?'
+end
+
+puts 'And let\'s give it up for Pascal!'
+puts '<crickets chirp in the distance>'
+
+puts ''
+
+3.times do
+	puts 'Hip-Hip-Horray!'
+	end
+
+puts '' 
+
+2.times do
+	puts '...you can say that again...'
+end
