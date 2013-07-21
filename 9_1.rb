654809a953cc45332fcdb47a369a708a2d9ce043
ofazomi <ofazomi@gmail.com>
2013-07-21 10:51:10 -0700

    Create 9_1.rb

---
+++ 9_1.rb
@@ -0,0 +1,22 @@
+def say_moo
+puts 'moooooo...'
+end
+x = 0
+
+while x <=2
+  say_moo
+	say_moo
+	say_moo
+	puts 'ching-ching'
+	x = x +1
+end
+
+puts
+
+def say_meh number_meh
+	puts 'meh ' * number_meh
+end
+
+say_meh 3
+puts 'so what'
+say_meh 1
