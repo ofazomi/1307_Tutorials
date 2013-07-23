1e134f730e437931c2c0342d6228b8d47c646d76
ofazomi <ofazomi@gmail.com>
2013-07-23 16:19:05 -0700

    Create 9_5b.rb

---
+++ 9_5b.rb
@@ -0,0 +1,58 @@
+puts 'Please provide a number to be converted to roman numerals.'
+num = gets.chomp.to_i
+
+if num >= 1000
+  m = num/1000
+puts array[0]
+puts array[1]
+
+	places = array.length.to_i
+
+	I = array[(places)]
+	X = array[(places - 1)]
+	C = array[(places - 2)]
+	M = array[(places - 3)]
+
+
+
+	puts 'between '
+array = num.split(//)
+
+num = gets.chomp.to_i
+
+if num >= 1000
+	m = num/1000
+
+
+
+	
+def roman num
+	while true
+		puts num.to_s
+		reply = gets.chomp.downcase
+		return true if reply == 'yes'
+		return false if reply == 'no'
+		puts 'Please answer "yes" or "no"'
+		
+	end
+end
+
+puts 'Here is my favorite parlor game.'
+sleep 2
+puts 'Name a year, any year.'
+num = gets.chomp
+
+while num.to_i.to_s != num
+    puts 'Come on, goof, a number, please.'
+    num = gets.chomp
+end
+    
+puts 'Just a second.'
+sleep 2
+num = num.to_i
+
+if (num%100 != 0 && num%4 == 0) || num%400 == 0
+    puts num.to_s + ' is a leap year.'
+    else puts num.to_s + ' is not a leap year.' 
+    end
+sleep 2
