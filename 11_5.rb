9ab6dd48f50fea239a4ad2f3c5f2f52a34a13dd6
ofazomi <ofazomi@gmail.com>
2013-07-31 19:54:25 -0700

    Create 11_5.rb

---
+++ 11_5.rb
@@ -0,0 +1,26 @@
+buffy_quote_1 = '\'Kiss rocks\'?
+Why would anyone want to kiss...
+Oh, wait. I get it.'
+buffy_quote_2 = "'Kiss rocks'?\n" +
+"Why would anyone want to kiss...\n" +
+"Oh, wait. I get it."
+puts buffy_quote_1
+puts
+puts(buffy_quote_1 == buffy_quote_2)
+
+puts
+puts "3...\n2...\n1...\nHAPPY NEW YEAR!"
+puts
+puts '3...\n2...\n1...\nHAPPY NEW YEAR!'
+
+puts
+puts 'single(\') and double (") quotes'
+puts "single (') and double (\") quotes"
+
+puts
+name = 'Jenny'
+phone =  '867-5309'
+
+puts "Name = #{name}, Phone = #{phone}"
+
+puts "#{2 * 10**4 + 1} Leagues Under the Sea, THE PREQUEL!"
