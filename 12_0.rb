e695b88840de402adb0d904dc0e90990eff6d69b
ofazomi <ofazomi@gmail.com>
2013-08-02 11:34:45 -0700

    Create 12_0.rb

---
+++ 12_0.rb
@@ -0,0 +1,12 @@
+#array addition
+alpha = Array.new + [12345]
+
+#string addition
+beta = String.new + 'hello'
+
+#time and date
+karma = Time.new
+
+puts "alpha = #{alpha}"
+puts "beta = #{beta}"
+puts "karma = #{karma}"
