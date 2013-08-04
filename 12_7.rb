c47803d71a945fd82641b979ece4e773a30879c3
ofazomi <ofazomi@gmail.com>
2013-08-04 09:48:10 -0700

    Create 12_7.rb

---
+++ 12_7.rb
@@ -0,0 +1,9 @@
+puts(42.class)
+puts("I've got class!".class)
+puts(Time.new.class)
+puts(Time.class)
+puts(String.class)
+
+# Hold your breath through the tunnel, boys and girls!
+puts(Class.class)
+# <gasp!>
